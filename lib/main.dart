f14f1ec3c3d7242495a5fd557fbc045d7deddce9
Paul Rudin <paul@rudin.co.uk>
2020-10-28 11:06:55 +0000

    alerts at end

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,23 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [ ];
+
+  void _press(bool selection, BuildContext context) {
+    setState((){
+        scoreKeeper.add(
+          quizBrain.getQuestionAnswer() == selection ?
+          Icon(Icons.check, color: Colors.green) :
+          Icon(Icons.close, color: Colors.red),
+        );
+
+        if (quizBrain.nextQuestion() == 0) {
+          Alert(context: context, title: "Quiz Finished", desc: "You've answered all the questions").show();          
+          scoreKeeper = [];
+        }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -60,9 +80,7 @@ class _QuizPageState extends State<QuizPage> {
                   fontSize: 20.0,
                 ),
               ),
-              onPressed: () {
-                //The user picked true.
-              },
+              onPressed: () => _press(true, context),
             ),
           ),
         ),
@@ -77,21 +95,76 @@ class _QuizPageState extends State<QuizPage> {
                   fontSize: 20.0,
                   color: Colors.white,
                 ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
+              ), //Text
+              onPressed: () => _press(false, context),
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper
+        ),
       ],
     );
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+class Question {
+  String question;
+  bool answer;
+
+  Question(String q, bool a) {
+    question = q;
+    answer = a;
+  }
+}
+
+class QuizBrain {
+  int _qnum = 0;  
+
+  List<Question> _questions = [
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+  String getQuestionText() {
+    return _questions[_qnum].question;
+  }
+
+  bool getQuestionAnswer() {
+    return _questions[_qnum].answer;
+  }
+
+  int numQuestions() => _questions.length;
+
+  int nextQuestion() {
+    if (_qnum < numQuestions() - 1 ) {
+      _qnum++;
+    } else {
+      _qnum = 0;
+    }
+    return _qnum;
+  }
+}
